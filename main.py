625029d08b17d33b368697dbb5cb6590b3619592
bobpham2008 <phamtuankien2k8@gmail.com>
2026-07-05 11:12:56 -0300

    Add loop to continuously add students

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 def add_student():
   student_name = input("Enter student name: ")
   print("Student:", student_name)
+
+while True:
+  add_student()
+  choice = input("Continue? (y/n):")
+  
+  if choice.lower() == "n":
+    print("Goodbye!")
+    break
+    
