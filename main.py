7b64a34f4eb2aa48cba7892372412b09c89a9244
bobpham2008 <phamtuankien2k8@gmail.com>
2026-07-08 16:58:08 -0300

    Implement student management features
    Added functionality to manage a student list, including adding and displaying students.

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 def add_student():
   student_name = input("Enter student name: ")
   student_name = student_name.strip().title()
-  print("Student:", student_name)
+  students_list.append(student_name)
+  print("Student added successfully!")
 
+def show_students():
+  print("\n===== STUDENT LIST =====")
+  
+  if len(students_list) == 0:
+    print("No students found.")
+    return
+    
+    for student in students_list:
+      print(Student)
+      
 while True:
   print("\n===== STUDENT MANAGEMENT =====")
   print("1. Add Student")
+  print("2. Show Students")
   print("0. Exit")
   
   choice = input("Choose:")
   if choice == "1":
     add_student()
+  elif choice == "2":
+    show_students()
   elif choice == "0":
     print("Goodbye!")
     break
     
   else:
-    print("Invalid choice. Please try again.")
+    print("Invalid choice.")
   
